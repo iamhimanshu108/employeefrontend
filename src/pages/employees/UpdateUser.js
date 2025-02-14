fc1b316c872ab4b937414384a9ac6c372d46c534
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2025-02-14 18:51:25 +0530

    updateminor

--- src/pages/employees/UpdateUser.js
+++ src/pages/employees/UpdateUser.js
@@ -1,74 +1,66 @@
-import React, { useEffect } from 'react';
-import "./UpdateUser.css";
-import { useState } from "react";
+import React, { useEffect, useState } from 'react';
 import { Button, Form } from "react-bootstrap";
-import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import "./UpdateUser.css";
 
 const UpdateUser = () => { 
-
-    const{id}  = useParams();
+    const { id } = useParams();
     const navigate = useNavigate();
-
     const [formData, setFormData] = useState({
-            name: "",
-            email: "",
-            phone: "",
-            department: "",
+        name: "",
+        email: "",
+        phone: "",
+        department: "",
+    });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
         });
-    
-        const handleInputChange = (event) => {
-            const { name, value } = event.target;
-            setFormData({
-                ...formData,
-                [name]: value,
-            });
-        };
+    };
 
-        const backendUrl = process.env.REACT_APP_API_URL || 'https://employeemanager-w22t.onrender.com';
+    const backendUrl = process.env.REACT_APP_API_URL || 'https://employeemanager-w22t.onrender.com';
 
-        useEffect(() => {
-            const fetchEmployee = async () => {
-                try {
-                    const response = await fetch(`${backendUrl}/api/employee/${id}`);
-                    const data = await response.json();
-                    setFormData(data);
-                } catch (error) {
-                    console.error("Error:", error);
-                }
-
-                
-            }
-
-            fetchEmployee();
-        }, [id,backendUrl]);
-
-        const handleSubmit = async (e) => {
-            e.preventDefault();
-            
+    useEffect(() => {
+        const fetchEmployee = async () => {
             try {
-                const response = await fetch(`${backendUrl}}/api/employee/${id}`, {
-                    method: "PATCH",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(formData),
-                });
-        
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-        
+                const response = await fetch(`${backendUrl}/api/employee/${id}`);
                 const data = await response.json();
-                console.log(data);
-                navigate("/");
+                setFormData(data);
             } catch (error) {
                 console.error("Error:", error);
             }
+        };
+
+        fetchEmployee();
+    }, [id, backendUrl]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${backendUrl}/api/employee/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log(data);
+            navigate("/");
+        } catch (error) {
+            console.error("Error:", error);
         }
+    };
 
     return (
-        <>
         <div className="center-form">
             <h1>Update Employee</h1>
             <Form onSubmit={handleSubmit}>
@@ -77,7 +69,7 @@ const UpdateUser = () => {
                 </Form.Group>
 
                 <Form.Group controlId="formBasicEmail">
-                    <Form.Control type="text" name="email" placeholder="Enter email" value={formData.email} onChange={handleInputChange} />
+                    <Form.Control type="email" name="email" placeholder="Enter Email" value={formData.email} onChange={handleInputChange} />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPhone">
@@ -88,11 +80,10 @@ const UpdateUser = () => {
                     <Form.Control type="text" name="department" placeholder="Enter Department" value={formData.department} onChange={handleInputChange} />
                 </Form.Group>
 
-                <Button variant="primary" type="submit" className="w-100"> Update Employee</Button>
+                <Button variant="primary" type="submit" className="w-100">Update Employee</Button>
             </Form>
         </div>
-        </>
-    )
-}
+    );
+};
 
 export default UpdateUser;
